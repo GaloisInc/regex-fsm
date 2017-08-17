e69beb353bc07a98e439e067bd03f3e872f2f3b8
Daniel Wagner <me@dmwit.com>
2017-08-17 15:04:58 -0700

    prototype tool for converting from JSON format to IBM's MBP format

---
+++ exe/json2ntl.hs
@@ -0,0 +1,106 @@
+module Main where
+
+import Control.Monad
+import Data.Aeson
+import Data.ByteString.Lazy (ByteString)
+import Data.Map (Map)
+import Data.Matrix
+import Data.Text (pack)
+import Regex.Types
+import System.Environment
+import System.Exit
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.Map as M
+
+sequentialPositions :: [Step] -> Bool
+sequentialPositions steps =
+  map position steps == zipWith (const . pack . show) [0..] steps
+
+sanityCheckAlphabet :: [Step] -> IO ()
+sanityCheckAlphabet steps = case map (M.keysSet . branches) steps of
+  [] -> die "No steps!"
+  s:ss -> id
+       . mapM_ warn
+       . filter (\(i, s') -> s /= s')
+       . zip [1..]
+       $ ss
+    where
+    warn (i, s') = putStrLn
+      $  "WARNING: Alphabet " ++ show s'
+      ++ " at position " ++ show i
+      ++ " does not match alphabet " ++ show s
+      ++ " at position 0."
+
+maxSize :: [Step] -> Int
+maxSize steps = maximum
+  [ dim
+  | Step { sDims = (r, c) } <- steps
+  , dim <- [r, c]
+  ]
+
+class ZeroPad a where zeroPad :: Int -> Int -> a -> a
+
+instance Num a => ZeroPad (Matrix a) where
+  -- special case: can't construct a zero-row matrix for padBottom
+  zeroPad r c m = (m <|> padRight) <-> padBottom where
+    padRight  = zero (nrows m) (c - ncols m)
+    padBottom = zero (r - nrows m) c
+
+instance ZeroPad v => ZeroPad (Map k v) where
+  zeroPad r c = fmap (zeroPad r c)
+
+instance ZeroPad Step where
+  zeroPad r c (Step _ pos bs) = Step (r,c) pos (zeroPad r c bs)
+
+instance ZeroPad a => ZeroPad [a] where
+  zeroPad r c = map (zeroPad r c)
+
+pprint :: [Step] -> String
+pprint         = bracket . concatMap pprintStep where
+  pprintStep   = bracket . concatMap pprintBranch . M.toAscList . branches
+  pprintBranch = bracket . concatMap pprintRow . toLists . snd
+  pprintRow    = bracket . unwords . map show
+  bracket s    = "[" ++ s ++ "]"
+
+convert :: ByteString -> IO ()
+convert bs = case eitherDecode bs of
+  Right (MBPInterpreter steps _) -> do
+    unless (sequentialPositions steps) $
+      putStrLn "WARNING: Ignoring non-sequential position information."
+    sanityCheckAlphabet steps
+    let size = maxSize steps
+    writeFile ("n" ++ show size ++ "_L" ++ show (length steps) ++ ".txt")
+              (pprint . zeroPad size size $ steps)
+  Left err -> die $ "Could not read matrix branching program:\n" ++ err
+
+usage :: IO String
+usage = do
+  progName <- getProgName
+  return $ "USAGE: " ++ progName ++ " [--] FILE\n"
+        ++ "   OR: " ++ progName ++ " -h\n"
+        ++ "   OR: " ++ progName ++ " --help\n"
+        ++ "\n"
+        ++ "Convert a matrix branching program in JSON format to NTL format.\n"
+        ++ "Use - to read from stdin. If you want to read from files named -, -h, or\n"
+        ++ "--help, insert -- before the filename.\n"
+        ++ "\n"
+        ++ "This conversion is lossy:\n"
+        ++ "* The information available in the JSON format about the meaning of the output\n"
+        ++ "  of the matrix branching program is lost outright.\n"
+        ++ "* The information about which input positions to inspect for each matrix is\n"
+        ++ "  lost; a warning is printed if the JSON has instructions to read the input in\n"
+        ++ "  any other order than front to back.\n"
+        ++ "* The information about the alphabet is lost. Matrices in each step of the\n"
+        ++ "  branching program are printed in Unicode codepoint order. A warning is printed\n"
+        ++ "  if the alphabet varies from step to step.\n"
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    ["-h"]       -> usage >>= putStrLn
+    ["--help"]   -> usage >>= putStrLn
+    ["-"]        -> LBS.getContents   >>= convert
+    ["--", file] -> LBS.readFile file >>= convert
+    [file]       -> LBS.readFile file >>= convert
+    _            -> usage >>= die
--- src/Regex/Types.hs
+++ src/Regex/Types.hs
@@ -12,15 +12,55 @@ import           Control.Monad.State
 import           Data.Aeson
 import           Data.Data
 import           Data.Default
+import qualified Data.HashMap.Strict as HM
 import           Data.Map            (Map)
 import qualified Data.Map            as M
 import           Data.Matrix
 import           Data.Monoid
 import           Data.Set            (Set, insert)
-import           Data.Text           (pack)
+import           Data.Text           (Text, pack)
 
 type Transition s a = Map s (Map (Maybe a) (Set s))
 type EpsClosure s = M.Map s (Set s)
+
+-- | A matrix branching program, together with information about how to
+-- interpret the output of the program.
+data MBPInterpreter = MBPInterpreter [Step] (Matrix String) deriving (Eq, Show)
+
+-- TODO: This FromJSON instance (and supporting ones) were stolen from cryfsm.
+-- We should write a library that cryfsm and this package both depend on that
+-- shares a few relevant types, instances, and utilities so that we don't have
+-- this code duplication.
+instance FromJSON MBPInterpreter where
+  parseJSON = withObject "matrix branching program" $ \o -> do
+    steps   <- o .: "steps"
+    when (null steps) (fail "program must have at least one step")
+    outputs <- o .: "outputs"
+    return (MBPInterpreter steps outputs)
+
+type Position   = Text
+type Dimensions = (Int, Int)
+
+-- constraints:
+-- * at least one matrix
+-- * all matrices have the same dimensions
+-- * does not branch on "position" -- this is semantically stupid, and a
+--   wart left over from bad JSON format design
+data Step = Step
+  { sDims    :: Dimensions
+  , position :: Position
+  , branches :: Map Char (Matrix Int)
+  } deriving (Eq, Show)
+
+instance FromJSON Step where
+  parseJSON = withObject "step" $ \o -> do
+    position <- o .: "position"
+    branches <- parseJSON (Object (HM.delete "position" o))
+    case M.elems branches of
+      [] -> fail "each step must have at least one branch"
+      b:bs | all (\b' -> dims b == dims b') bs -> return (Step (dims b) position branches)
+           | otherwise -> fail "all branches in the step must have the same dimensions"
+    where dims matrix = (nrows matrix, ncols matrix)
 
 -- | Type used to convert into json output
 data Matrices = Matrices Int [ Map Char (Matrix Int) ]
@@ -40,6 +80,15 @@ instance ToJSON Matrices where
 
 instance ToJSON (Matrix Int) where
   toJSON = toJSON . toLists
+
+instance FromJSON field => FromJSON (Matrix field) where
+  parseJSON v = do
+    elems <- parseJSON v
+    let rows = length elems
+        cols:colss = map length elems
+    if rows > 0 && all (cols==) colss
+       then return (fromLists elems)
+       else fail "expected a non-empty rectangular array of arrays"
 
 -- | AST for Regular Expressions
 data Reg a
